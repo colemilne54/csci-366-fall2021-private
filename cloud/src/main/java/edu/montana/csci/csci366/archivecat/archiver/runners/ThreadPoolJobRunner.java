45287027ec4c9bb12318914c6065f0db88f0bb28
Cole Milne <colemilne54@gmail.com>
2021-12-14 12:34:48 -0700

    cloud proj done

--- cloud/src/main/java/edu/montana/csci/csci366/archivecat/archiver/runners/ThreadPoolJobRunner.java
+++ cloud/src/main/java/edu/montana/csci/csci366/archivecat/archiver/runners/ThreadPoolJobRunner.java
@@ -3,6 +3,7 @@ package edu.montana.csci.csci366.archivecat.archiver.runners;
 import edu.montana.csci.csci366.archivecat.archiver.jobs.DownloadJob;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -11,27 +12,21 @@ public class ThreadPoolJobRunner implements DownloadJobRunner {
         // TODO implement - use a ThreadPoolExecutor with 10 threads to execute the jobs
 
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+        int count = downloadJobs.size();
+        CountDownLatch latch = new CountDownLatch(count);
 
-//        still have to gate it
-//        use countdown latch to make sure still
-//        executor.
+        for (DownloadJob downloadJob : downloadJobs) {
+            executor.execute(() -> {
+                downloadJob.run();
+                latch.countDown();
+            });
+        }
+        try {
+            latch.await();
+            System.out.println("----done----");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-//        int count = downloadJobs.size();
-//        CountDownLatch latch = new CountDownLatch(count);
-//        for (DownloadJob downloadJob : downloadJobs) {
-//            var t = new Thread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    downloadJob.run();
-//                    latch.countDown();
-//                }
-//            });
-//            t.start();
-//            try {
-//                latch.await();
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
     }
 }
