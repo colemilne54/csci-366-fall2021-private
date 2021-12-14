2b35434d67869b265bb0b61fe4ea19910acdf050
Cole Milne <colemilne54@gmail.com>
2021-12-13 19:30:55 -0700

    jobs test and archive test passing

--- cloud/src/main/java/edu/montana/csci/csci366/archivecat/archiver/jobs/ImageDownloadJob.java
+++ cloud/src/main/java/edu/montana/csci/csci366/archivecat/archiver/jobs/ImageDownloadJob.java
@@ -21,16 +21,28 @@ public class ImageDownloadJob extends AbstractDownloadJob {
         // TODO implement - hint: use the 'Content-Type' header of the
         //                  response to determine the type of image and
         //                  give it the correct file ending
+
+        var baseName = getArchive().computeSHA1(getURL());
+
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(URI.create(getURL())).build();
+        HttpResponse<byte[]> response = client.send(request, HttpResponse.BodyHandlers.ofByteArray());
+
+        String contentTypeHeader = response.headers().firstValue("Content-Type").get();
+        String[] splitStr = contentTypeHeader.split("/");
+        String imgType = splitStr[1];
+
+        _fullPathToImage = getArchive().saveFile(baseName + "." + imgType, response.body());
     }
 
     @Override
     public void updateElement() {
-        // TODO implement
+        getElement().attr("href", "/" + _fullPathToImage);
     }
 
     @Override
     protected String getURL() {
-        // TODO implement
-        return "";
+//        TODO change src?
+        return getElement().attr("abs:src");
     }
 }
